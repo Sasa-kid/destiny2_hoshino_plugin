f82d519f63304bde34ebc58298745b6851a21aee
Sasa-kid <68634616+Sasa-kid@users.noreply.github.com>
2021-09-27 13:11:48 +0800

    Update and rename get_zhu_info.py to get_today_info.py

--- get_today_info.py
+++ get_today_info.py
@@ -1,21 +1,21 @@
 import re
 import requests
 
-# 获取蛛王链接
-def getzhuHtml(url):
+# 获取日报链接
+def gettodayHtml(url):
   html4 = requests.get(url)
   html4.encoding = 'utf-8'
   html4 = html4.text
   return html4
 
-# 获取蛛王图片链接的字符串
-def getzhuImg(html4):
+# 获取日报图片链接的字符串
+def gettodayImg(html4):
   imglist = re.findall(r'(https\:\/\/cdn\.max\-c\.com\/heybox\/dailynews\/img\/(?!c4f5035d1b8053c400c72c0656c12d97).+?\.jpg|https\:\/\/cdn\.max\-c\.com\/heybox\/dailynews\/img\/(?!c4f5035d1b8053c400c72c0656c12d97).+?\.png)', html4)
   for url4 in imglist:
     return url4
 
 def sethtml4():
-  html4 = str(getzhuHtml("https://api.xiaoheihe.cn/wiki/get_article_for_app/?article_id=8829978&wiki_id=1085660&is_share=1"))
+  html4 = str(gettodayHtml("http://www.tianque.top/d2api/today/"))
   return html4
 
-# print(getzhuImg(sethtml4()))
+# print(gettodayImg(sethtml4()))
