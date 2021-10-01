88f9c31483dab7d51d0c0d93e8b6e7e42721fd56
Sasa-kid <68634616+Sasa-kid@users.noreply.github.com>
2021-10-02 03:05:44 +0800

    Update get_xur_info.py

--- get_xur_info.py
+++ get_xur_info.py
@@ -1,21 +1,42 @@
 import re
 import requests
 
-# 获取老九链接
+# 返回小黑盒百科的源码
 def getxurHtml(url):
-  html2 = requests.get(url)
+  html222 = requests.get(url)
+  html222.encoding = 'utf-8'
+  html222 = html222.text
+  html222 = html222.replace('\n','')
+  html222 = html222.replace(' ','')
+  # print(html222)
+  return html222
+
+# 返回老九的链接
+def getxur(html222):
+  imglist = re.findall(r'Xur\\/\\u82cf\\u5c14\\/\\u8001\\u4e5d.+?id\=1085660', html222)
+  for html20 in imglist:
+    imglist = re.findall(r'https\:\\\/\\\/api\.xiaoheihe\.cn\\\/wiki\\\/get\_article\_for\_app.+?id\=1085660', html20)
+    for html22 in imglist:
+      html22 = html22.replace('\\','')
+      # print(html22)
+      return html22
+
+# 返回老九的源码
+def getxurHtml2(html22):
+  html2 = requests.get(html22)
   html2.encoding = 'utf-8'
   html2 = html2.text
+  # print(html2)
   return html2
 
-# 获取老九图片链接的字符串
+# 字符串格式输出图片链接
 def getxurImg(html2):
-  imglist = re.findall(r'(https\:\/\/cdn\.max\-c\.com\/heybox\/dailynews\/img\/(?!c4f5035d1b8053c400c72c0656c12d97).+?\.jpg|https\:\/\/cdn\.max\-c\.com\/heybox\/dailynews\/img\/(?!c4f5035d1b8053c400c72c0656c12d97).+?\.png)', html2)
+  imglist = re.findall(r'https\:\/\/cdn\.max\-c\.com\/heybox\/dailynews\/img\/(?!c4f5035d1b8053c400c72c0656c12d97).+?\.png|https\:\/\/cdn\.max\-c\.com\/heybox\/dailynews\/img\/(?!c4f5035d1b8053c400c72c0656c12d97).+?\.jpg', html2)
   for url2 in imglist:
     return url2
 
 def sethtml2():
-  html2 = str(getxurHtml("https://api.xiaoheihe.cn/wiki/get_article_for_app/?article_id=9711351&wiki_id=1085660&is_share=1"))
+  html2 = str(getxurHtml2(str(getxur(str(getxurHtml("https://api.xiaoheihe.cn/wiki/get_homepage_content/?wiki_id=1085660&verison=&is_share=1"))))))
   return html2
 
 # print(getxurImg(sethtml2()))
